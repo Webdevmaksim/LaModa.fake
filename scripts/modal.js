c5301898369be7cf068ccaf4b96bf13c56e4475f
Webdevmaksim <maksim.saraev.121.5@gmail.com>
2021-07-06 13:12:42 +0300

    Отключение скрола + вызов модалки

--- scripts/modal.js
+++ scripts/modal.js
@@ -1,16 +1,44 @@
 'use strict';
 
+
+
 const subheaderCart = document.querySelector('.subheader__cart');
 const cartOverlay = document.querySelector('.cart-overlay');
+
+/* Блокировка скрола = начало */
+
+// Отключение скрола
+const disableScroll = () => {
+
+    const widthScroll = window.innerWidth - document.body.offsetWidth;
+
+    document.body.style.cssText = `
+    postion: fixed;
+    top: ${-window.scrollY}px;
+    left: 0;
+    width: 100%;
+    height: 100vh;
+    overflow: hidden;
+    padding-right: ${widthScroll}px;
+    `;
+};
+
+// Включение скрола
+const enableScroll = () => {
+    document.body.style.cssText = '';
+};
+/* Блокировка скрола = конец */
 
 // Функция открытия модалки
 const cartModalOpen = () => {
     cartOverlay.classList.add('cart-overlay-open');
+    disableScroll();
 };
 
 // Функция закрытия модалки
 const cartModalClose = () => {
     cartOverlay.classList.remove('cart-overlay-open');
+    enableScroll();
 };
 
 // Событие клика на кнопку корзины
@@ -20,6 +48,7 @@ subheaderCart.addEventListener('click', cartModalOpen);
 document.addEventListener('keydown', event => {
     if (event.code === 'Escape'){
         cartModalClose();
+        enableScroll();
     }
 });
 
